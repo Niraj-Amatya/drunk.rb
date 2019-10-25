912557b6d62070b42b8760a4fdf966154e4aa9a4
Niraj <amatyaraj@gmail.com>
2019-10-25 22:32:26 +1100

    pseudocode with some code added

---
+++ drunk.rb
@@ -0,0 +1,15 @@
+# conditional
+    #First condition to get access to bar is age should be more or equal to 18. Thi should
+    #be met before going to next condition.
+    #Second condition is he/she should not be drunk.
+
+
+#Variable age with age equal to 18
+age = 18
+
+#prints this line to the user
+puts "Enter your age?"
+
+#Getting user input for the age
+age = gets.chomp.to_i
+
