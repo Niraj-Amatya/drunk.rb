1502d19c42dc461ba04cd0f9b54d515f25a04553
Niraj <amatyaraj@gmail.com>
2019-10-25 22:46:27 +1100

    condition when age is less than 18

--- drunk.rb
+++ drunk.rb
@@ -15,12 +15,23 @@ age = gets.chomp.to_i
 
 #first condition if age greater or less than 18
 if age >= 18
+    #asking if user is drunk. Answer is true, if drunk and false, if not.
     puts "Are you drunk?"
-    drunk = gets.chomp
+    puts "Enter true: if drunk and false: if not drunk."
+
+     #gets.chomp converts boolean true and false to string 'true' and 'false'
+    #so to solve this problem, drunk is given boolean value false when user input is string 'false'
+    #in line 24
+    drunk = gets.chomp 
     if drunk == "false"
         drunk == false
         puts "You are allowed."
     else drunk == "true"
-        drunk == true
+        drunk == true #drunk is given boolean value true when user input is string 'true'
         puts "You are not allowed"
+    end
+#if age is less than 18
+else age < 18
+    puts "You are not in the legal age to go in."
+
 end
